913d9709a43f109e95b76980cf35314663b189e1
Surajsonkar07 <sonkarsuraj447@gmail.com>
2026-01-15 03:03:57 +0530

    chore: aggressive build size optimization (excludes + npmrc)

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,17 @@ const nextConfig = {
   experimental: {
     outputFileTracingExcludes: {
       '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
+        './**/node_modules/@swc/core-linux-x64-gnu',
+        './**/node_modules/@swc/core-linux-x64-musl',
+        './**/node_modules/@esbuild/linux-x64',
+        './**/node_modules/webpack',
+        './**/node_modules/terser',
+        './**/node_modules/typescript',
+        './**/node_modules/eslint',
+        './**/node_modules/jest',
+        './**/node_modules/@types',
+        './**/*.map',
+        './**/*.d.ts',
       ],
     },
   },
